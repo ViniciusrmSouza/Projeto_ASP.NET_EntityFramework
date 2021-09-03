8df5944980d7f1d57d538e9e027b8c4ddf097337
Vinicius Souza <viniciusrmsouza577@gmail.com>
2021-09-03 16:37:08 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -1,9 +1,31 @@
-﻿
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
 namespace SalesWebMvc.Models
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Saller> Sallers { get; set; } = new List<Saller>();//Implementar o tipo mais generico da coleção
+
+        public Departament()
+        {
+        }
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Saller saller)
+        {
+            Sallers.Add(saller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sallers.Sum(saller => saller.TotalSales(initial, final));//retorna a soma de todas as vendas do metodo TotalSales de todos Sallers na lista
+        }
     }
 }
